c3ef85770a112dac09baba821d9595e8a4972b00
David Pontes <pondavid103@gmail.com>
2024-06-01 19:23:09 -0300

    add:project

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
@@ -12,14 +13,36 @@ Pessoa p2 = new Pessoa(nome: "Hóspede 2");
 hospedes.Add(p1);
 hospedes.Add(p2);
 
-// Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+try
+{
+    reserva.CadastrarHospedes(hospedes);
+    Console.WriteLine("Hóspedes cadastrados com sucesso!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao cadastrar hóspedes: {ex.Message}");
+}
+
+if (reserva.Hospedes != null)
+{
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    
+    // Verifica se a capacidade da suíte é maior ou igual ao número de hóspedes
+    if (reserva.ObterQuantidadeHospedes() <= suite.Capacidade)
+    {
+        Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+    }
+    else
+    {
+        Console.WriteLine("Não é possível calcular o valor da diária. A capacidade da suíte é menor que o número de hóspedes.");
+    }
+}
+else
+{
+    Console.WriteLine("Não há hóspedes na reserva.");
+}
